fd6b1dcbfde005c023301ea5b4cfc5b6270d3252
Santiago <santiago.archila@gmail.com>
2013-10-28 17:34:06 -0700

    _.every passes all tests

--- src/underbar.js
+++ src/underbar.js
@@ -190,6 +190,17 @@ var _ = { };
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    var argNo = arguments.length;
+    
+    return _.reduce(collection,function(allMatch, item){
+      if (!allMatch){
+        return false;
+      } else if (argNo<2){
+        return Boolean(item);
+      } else {
+        return Boolean(iterator(item));
+      }
+    },true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
