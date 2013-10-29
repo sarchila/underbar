653f27e3ea235f69a4e72be99eed368ac5fe57fc
Santiago <santiago.archila@gmail.com>
2013-10-29 10:13:48 -0700

    _.defaults passes all tests

--- src/underbar.js
+++ src/underbar.js
@@ -251,6 +251,14 @@ var _ = { };
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments,function(eachObj){
+      _.each(eachObj,function(value,key){
+        if (!_.contains(Object.keys(obj),key)){
+          obj[key] = value;
+        }
+      });
+    });
+    return obj;
   };
 
 
